7f561df2d71b82f931f74058d2eeef38095d0f01
GUIBINGBING <bingbing.gui@outlook.com>
2025-11-09 21:47:55 +0900

    Microsoft-Agent-Framework

---
+++ src/09-AI-Agent/Agent-Framework/Agent-Running/Program.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+// This sample shows how to create and use a simple AI agent with Azure OpenAI as the backend.
+
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using OpenAI;
+using System.Text;
+
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+var deploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+AIAgent agent = new AzureOpenAIClient(
+    new Uri(endpoint),
+    new AzureCliCredential())
+    .GetChatClient(deploymentName)
+    .CreateAIAgent(instructions: "你是一个诗人", name: "Joker");
+
+// Invoke the agent and output the text result.
+Console.WriteLine(await agent.RunAsync("请帮我写一首诗。"));
+
+Console.ReadLine();
+// Invoke the agent with streaming support.
+//await foreach (var update in agent.RunStreamingAsync("请帮我写一首诗。"))
+//{
+//    Console.WriteLine(update);
+//}
